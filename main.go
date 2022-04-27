0a262fc407f3589231cab147b3af0f9fe5fb336b
Kirill Scherba <kirill@scherba.ru>
2022-04-27 03:40:23 +0300

    Get Starred repos with gh utilite; parameter -printonly added

--- main.go
+++ main.go
@@ -14,12 +14,14 @@
 //   -users  <[user-or-organisation-comma-separated-list]>
 //   -limit  [user-repo-comma-separated-list]
 //   -output [local-folder-name], default: ./repos
+//   -printonly
 //   -starsonly
-//   -stars  
+//   -stars
 //
 // Usage examples:
 //
 //   go run . -users=kirill-scherba -limit=kirill-scherba/teonet-go -output=./tmp
+//   go run . -users=kirill-scherba -stars -output=./tmp
 //
 package main
 
@@ -27,9 +29,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os/exec"
 	"strings"
 )
@@ -38,29 +38,36 @@ func main() {
 
 	// Parse parameters
 	var userslist, limitslist, output, maxrepo string
-	var stars, starsonly bool
+	var stars, starsonly, printonly bool
+	//
 	flag.StringVar(&userslist, "users", "", "user or organisation comma separated list")
 	flag.StringVar(&limitslist, "limit", "", "user/repository comma separated list to backup, all if empty")
 	flag.StringVar(&output, "output", "repos", "local folder name to save repositories")
 	flag.BoolVar(&stars, "stars", false, "backup starred repositories also")
 	flag.BoolVar(&starsonly, "starsonly", false, "backup starred repositories only")
 	flag.StringVar(&maxrepo, "maxrepo", "1000", "maximum number of users repositories to be cloned")
+	flag.BoolVar(&printonly, "printonly", false, "print repositories but does not clone it")
 	flag.Parse()
 
 	// Parse users and limit
-	users := strings.Split(userslist, ",")
 	var limit []string
+	users := strings.Split(userslist, ",")
 	if len(strings.TrimSpace(limitslist)) != 0 {
 		limit = strings.Split(limitslist, ",")
 	}
 
 	// Get list of repos with gh cli application
+	var repos []string
 	for _, user := range users {
 		if !starsonly {
-			getRepos(output, strings.TrimSpace(user), maxrepo, limit)
+			r := getRepos(output, strings.TrimSpace(user), maxrepo, limit,
+				printonly)
+			repos = append(repos, r...)
 		}
 		if stars || starsonly {
-			getStars(output, strings.TrimSpace(user), maxrepo, limit)
+			r := getStars(output, strings.TrimSpace(user), maxrepo, limit,
+				printonly)
+			repos = append(repos, r...)
 		}
 	}
 }
@@ -69,7 +76,8 @@ func main() {
 var reponum int
 
 // getRepos get list of reopsitories and clone it
-func getRepos(dir, user, maxrepo string, limit []string) (repos []string) {
+func getRepos(dir, user, maxrepo string, limit []string,
+	printonly bool) (repos []string) {
 
 	// Get list of reopsitories with gh
 	out, err := exec.Command("gh", "repo", "list", user, "-L", maxrepo).Output()
@@ -91,53 +99,56 @@ func getRepos(dir, user, maxrepo string, limit []string) (repos []string) {
 	}
 
 	// Clone repos
-	cloneRepos(repos, limit, dir)
-
-	return
+	return cloneRepos(repos, limit, dir, printonly)
 }
 
 // getStars get list of starred reopsitories and clone it
-func getStars(dir, user, maxrepo string, limit []string) (repos []string) {
+func getStars(dir, user, maxrepo string, limit []string,
+	printonly bool) (repos []string) {
 
-	// Get stars by github api
-	resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/starred?page=1&per_page=%s", user, maxrepo))
-	if err != nil {
+	// Get list of starred reopsitories with gh by api
+	// Loop through pages with 100 entries per page
+	for p := 1; ; p++ {
+		endpoint := fmt.Sprintf("/users/%s/starred?per_page=100&page=%d",
+			user, p)
+		out, err := exec.Command("gh", "api", endpoint).Output()
 		if err != nil {
-			log.Printf("Can't get starred repos of %s: %s", user, err)
+			log.Fatal(err)
+		}
+
+		// Umarshal github api output
+		type starsData struct {
+			FullName string `json:"full_name,omitempty"`
+		}
+		var jsonData []starsData
+		if err := json.Unmarshal(out, &jsonData); err != nil {
+			log.Printf("Can't parse response body to json: %s\n%s", err,
+				string(out))
 			return nil
 		}
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("Can't read response body: %s", err)
-		return nil
-	}
 
-	// Umarshal github api output
-	type starsData struct {
-		FullName string `json:"full_name,omitempty"`
-	}
-	var jsonData []starsData
-	if err := json.Unmarshal(body, &jsonData); err != nil {
-		log.Printf("Can't parse response body to json: %s\n%s", err, string(body))
-		return nil
-	}
+		// Exit form loop
+		if len(jsonData) == 0 {
+			break
+		}
 
-	// Parse github api output
-	for i := range jsonData {
-		repos = append(repos, jsonData[i].FullName)
+		// Parse github api output
+		for i := range jsonData {
+			repos = append(repos, jsonData[i].FullName)
+		}
 	}
 
 	// Clone repos
-	cloneRepos(repos, limit, dir)
-
-	return
+	return cloneRepos(repos, limit, dir, printonly)
 }
 
 // cloneRepos from list of full repo name
-func cloneRepos(repos []string, limit []string, dir string) (cloned []string) {
+func cloneRepos(repos []string, limit []string, dir string,
+	printonly bool) (cloned []string) {
+
 	for _, repo := range repos {
-		// All if 'limit' slice empty or if 'repo' exists in 'limit' slice
+		// Get all repos if 'limit' slice is empty or get 'repo' exists in
+		// 'limit' slice
 		if !(len(limit) == 0 || inSlise(repo, limit)) {
 			continue
 		}
@@ -145,19 +156,27 @@ func cloneRepos(repos []string, limit []string, dir string) (cloned []string) {
 		// Print repo name
 		reponum++
 		fmt.Printf("repo %3d: %s\n", reponum, repo)
-		repos = append(repos, repo)
+
+		// Skip clone if printonly flag set
+		if printonly {
+			continue
+		}
 
 		// Clone repo
-		_, err := exec.Command("git", "clone", "--mirror", "git@github.com:"+repo+".git", dir+"/"+repo+".git").Output()
+		err := exec.Command("git", "clone", "--mirror", "git@github.com:"+repo+
+			".git", dir+"/"+repo+".git").Run()
 		if err != nil {
 			log.Fatal(err)
 		}
+		cloned = append(cloned, repo)
 
 		// Clone wiki repo
-		err = exec.Command("git", "clone", "--mirror", "git@github.com:"+repo+".wiki.git", dir+"/"+repo+".wiki.git").Run()
+		err = exec.Command("git", "clone", "--mirror", "git@github.com:"+repo+
+			".wiki.git", dir+"/"+repo+".wiki.git").Run()
 		if err != nil {
-			// log.Println(err)
+			continue
 		}
+		cloned = append(cloned, repo+".wiki")
 	}
 	return
 }
